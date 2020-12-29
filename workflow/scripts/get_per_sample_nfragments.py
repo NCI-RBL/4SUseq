b68da6734eb8e0897636e5b6a93d7a401078b43f
Vishal N Koparde <vishal.koparde@nih.gov>
2020-12-29 16:57:27 -0500

    get per sample fragments script grep fix

--- workflow/scripts/get_per_sample_nfragments.py
+++ workflow/scripts/get_per_sample_nfragments.py
@@ -2,14 +2,21 @@ import subprocess
 import json
 import sys
 
+def get_nreads_from_labels(filename,samplename):
+	f=open(filename)
+	for l in f.readlines():
+		l=l.strip().split("\t")
+		if l[0]=="#" and l[1]==samplename:
+			return l[2]
+
 samplename=sys.argv[1]  #sample name
 outfile=sys.argv[2]     #output json name.... sample.json
 data=dict()
 data['samplename']=samplename
 data['nfragments']=dict()
-data['nfragments']['raw'] = int(subprocess.check_output("grep \b"+samplename+"\b raw_fastq/lanes.txt|grep ^#|awk '{print $3}'",shell=True).strip())
-data['nfragments']['trim'] = int(subprocess.check_output("grep \b"+samplename+"\b trim/lanes.txt|grep ^#|awk '{print $3}'",shell=True).strip())
-data['nfragments']['fastuniq'] = int(subprocess.check_output("grep \b"+samplename+"\b fastuniq/lanes.txt|grep ^#|awk '{print $3}'",shell=True).strip())
+data['nfragments']['raw'] = get_nreads_from_labels("raw_fastq/lanes.txt",samplename)
+data['nfragments']['trim'] = get_nreads_from_labels("trim/lanes.txt",samplename)
+data['nfragments']['fastuniq'] = get_nreads_from_labels("fastuniq/lanes.txt",samplename)
 data['nfragments']['post_secondary_supplementary_filter'] = int(float(subprocess.check_output("grep properly hisat2/"+samplename+".post_secondary_supplementary_filter.bam.flagstat|awk '{print $1/2}'",shell=True).strip()))
 data['nfragments']['post_insertion_filter'] = int(float(subprocess.check_output("grep properly hisat2/"+samplename+".post_insertion_filter.bam.flagstat|awk '{print $1/2}'",shell=True).strip()))
 data['nfragments']['toSNPcalling'] = int(float(subprocess.check_output("grep properly hisat2/"+samplename+".toSNPcalling.bam.flagstat|awk '{print $1/2}'",shell=True).strip()))
