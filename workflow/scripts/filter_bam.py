d513f741407a86d9b629d418ca6540f58adc7824
Vishal N Koparde <vishal.koparde@nih.gov>
2020-12-31 09:36:11 -0500

    filter_bam.py now filters reads with insertions ... no need to use bbtools

--- workflow/scripts/filter_bam.py
+++ workflow/scripts/filter_bam.py
@@ -1,10 +1,18 @@
 import pysam,sys
 import argparse
 
+def get_ninsertions(ctuples):
+	for t in ctuples:
+		operation,length=t
+		if operation==1: # operation == 1 is insertion (see https://pysam.readthedocs.io/en/latest/api.html#pysam.AlignedSegment.get_cigar_stats)
+			return length
+	return 0
+
 parser = argparse.ArgumentParser(description='filter PE bamfile by mapQ values')
 parser.add_argument('-i',dest='inBam',required=True,help='Input Bam File')
 parser.add_argument('-o',dest='outBam',required=True,help='Output Bam File')
-parser.add_argument('-q',dest='mapQ',type=int,required=False,help='mapQ value ... default 6',default=6)
+parser.add_argument('-q',dest='mapQ',type=int,required=False,help='mapQ value ... default 6, maqQ of 5 or lower will be discarded',default=6)
+parser.add_argument('-n',dest='ninsertion',type=int,required=False,help='number of insertions allowed ... default 6... reads with 5 or fewer insertions will be kept',default=6)
 args = parser.parse_args()
 samfile = pysam.AlignmentFile(args.inBam, "rb")
 mapq=dict()
@@ -18,6 +26,8 @@ for read in samfile.fetch():
 	if read.is_duplicate:
 		continue
 	if read.is_proper_pair:
+		if get_ninsertions(read.cigartuples) >= args.ninsertion:
+			continue
 		if read.mapping_quality < args.mapQ and read.query_name in mapq:
 			del mapq[read.query_name]
 		if read.mapping_quality >= args.mapQ:
